c6279e50554ca1759788259f0cb5d465c68227ba
Baker El Muhur <b.elmuhur@gmail.com>
2022-05-24 13:17:29 +0200

    PRO-1: changed dropdown so it shows current student progress

--- src/app/codelab-overview/codelab-overview.component.ts
+++ src/app/codelab-overview/codelab-overview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {CodelabService} from "../services/codelab.service";
 import {Observable} from "rxjs";
 import {Codelab} from "../models/Codelab";
+import {CodelabProgress} from "../models/CodelabProgress";
 
 @Component({
   selector: 'app-codelab-overview',
@@ -9,12 +10,13 @@ import {Codelab} from "../models/Codelab";
   styleUrls: ['./codelab-overview.component.css']
 })
 export class CodelabOverviewComponent implements OnInit {
-  codelabs!: Observable<Codelab[]>
+  codelabs!: Observable<Codelab[]>;
+  codelabProgress: string[] = Object.values(CodelabProgress);
 
   constructor(private codelabService: CodelabService) { }
 
   ngOnInit(): void {
-    this.codelabs = this.codelabService.getCodelabsForStudent('1');
+    this.codelabs = this.codelabService.getCodelabsForStudent('1'); // change this later to logged in student id
   }
 
 }
