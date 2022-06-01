b8e6be5f62bb20468d9fdcea14c6d08240fda74b
Rinaldo <menichettir@hotmail.com>
2022-06-01 10:07:56 +0200

    Keycloack modifications

--- src/app/services/codelab.service.ts
+++ src/app/services/codelab.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
 import {Codelab} from "../models/Codelab";
+import {ProgressOverview} from "../models/ProgressOverview";
 
 @Injectable({
   providedIn: 'root'
