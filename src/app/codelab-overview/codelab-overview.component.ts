571b2590ea2cc86564289fb556174adc8ffde754
Alperen2112 <a.alperen@outlook.com>
2022-06-01 15:47:11 +0200

    ACC-1 added register component and form

--- src/app/codelab-overview/codelab-overview.component.ts
+++ src/app/codelab-overview/codelab-overview.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CodelabService} from "../services/codelab.service";
 import {LoginService} from "../services/login/login.service";
 import {CodelabProgress} from "../models/CodelabProgress";
-import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-codelab-overview',
