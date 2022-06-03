673aed55fd3821a920b3d15ffaf48ee4aeb9d798
Ruben Lommelen <ruben.lommelen@gmail.com>
2022-06-03 11:06:37 +0200

    ACC-1: Added validators to form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
-import {FormBuilder} from "@angular/forms";
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -10,10 +10,10 @@ import {FormBuilder} from "@angular/forms";
 export class RegisterComponent implements OnInit {
 
   registerForm = this.formBuilder.group({
-    email: '',
-    displayName: '',
-    password:  '',
-    repeatPassword:''
+    email: ['', [Validators.email, Validators.required]],
+    displayName: ['', [Validators.required]],
+    password:  ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@$%^&(){}:;<>,.?/~_+=|])[A-Za-z0-9!@$%^&(){}:;<>,.?/~_+-=|].{7,}$')]],
+    repeatPassword: ['', [Validators.required]]
   });
 
 
